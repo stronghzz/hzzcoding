6882ea03f578673087741d38fc028d1c8522e8b6
壮壮 贺 <760767745@qq.com>
2019-05-25 23:58:52 +0800

    practice

---
+++ 2019_5_25练习题/2019_5_25练习题/test.cpp
@@ -0,0 +1,52 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int w;
+//	int h;
+//	int count = 0;
+//	int flag1 = 0;
+//	int flag2 = 0;
+//	scanf("%d %d", &h, &w);
+//	for (int i = 0; i < h; i++)
+//	{
+//
+//		flag1++;
+//		if (flag1 == 3)
+//		{
+//			i++;
+//			flag1++;
+//		}
+//		for (int j = 0; j < w; j++)
+//		{
+//			flag2++;
+//			if (flag2 == 3)
+//			{
+//				j++;
+//				flag2 = 0;
+//			}
+//			count++;
+//
+//
+//		}
+//	}
+//	printf("%d",count);
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c, d;
+//	cin >> a >> b >> c >> d;
+//	int A = (a + c) / 2;
+//	int C = (d - b) / 2;
+//	int B1 = (c - a) / 2;
+//	int B2 = (b + d) / 2;
+//	if (B1 != B2)
+//		cout << "No";
+//	else
+//		cout << A << " " << B1 << " " << C;
+//	return 0;
+//}
