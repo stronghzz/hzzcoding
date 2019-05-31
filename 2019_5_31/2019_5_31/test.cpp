eee20a31c2d1116d1ea8dba954475f6b61be0deb
壮壮 贺 <760767745@qq.com>
2019-05-31 23:15:58 +0800

    practice

---
+++ 2019_5_31/2019_5_31/test.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+//
+//int foo(int n)
+//{
+//	if (n < 2)
+//	{
+//		return n;
+//	}
+//	else
+//	{
+//		return 2 * foo(n - 1) + foo(n - 2);
+//	}
+//}
+//
+//
+//
+//
+//int main()
+//{
+//	int ret = foo(5);
+//	
+//
+//
+//	return 0;
+//}
+
+
+//
+//class A
+//{
+//public:
+//	void foo()
+//	{
+//		cout << "1";
+//	}
+//	virtual void fun()
+//	{
+//		cout << "2";
+//	}
+//};
+//class B:public A
+//{
+//public:
+//	void foo()
+//	{
+//		cout << "3";
+//	}
+//	void fun()
+//	{
+//		cout << "4";
+//	}
+//};
+//
+//int main()
+//{
+//	B pb;
+//	A *pa =(A*)( &pb);//¿´ÀàÐÍ
+//	pa->foo();
+//	pa->fun();
+//
+//
+//	return 0;
+//}
