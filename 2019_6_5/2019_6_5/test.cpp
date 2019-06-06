a0b995ef2a71be508c5dd12dca1dfcee1fcda381
壮壮 贺 <760767745@qq.com>
2019-06-06 19:15:53 +0800

    practice

---
+++ 2019_6_5/2019_6_5/test.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+//static int a = 1;
+//void fun1(void) { a = 2; }
+//void fun2(void) { int a = 3; }
+//void fun3(void) { static int a = 4; }
+//int main()
+//{
+//	printf("%d", a);
+//	fun1();
+//	printf("%d", a);
+//	fun2();
+//	printf("%d", a);
+//	fun3();
+//	printf("%d", a);//初始化只能一次
+//
+//
+//
+//	return 0;
+//}
+//class A
+//{
+//public:
+//	virtual void foo()
+//	{
+//		printf("a");
+//	}
+//};
+//class B :public A
+//{
+//	virtual void foo()
+//	{
+//		printf("b");
+//	}
+//};
+//int main()
+//{
+//	A *a = new B;
+//	a->foo();
+//	delete a;
+//	return 0;
+//}
+
