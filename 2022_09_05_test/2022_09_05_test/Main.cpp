083f10f33807f97ca06d6e0fd74b3ea7c7f99f60
stronghzz <760767745@qq.com>
2022-09-05 23:12:25 +0800

    练习代码

---
+++ 2022_09_05_test/2022_09_05_test/Main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <stdlib.h>
+#include<malloc.h>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	//int y = 9;
+	//int z = 10;
+	//int a_7 = 1;
+	//int _a = 1;
+	//int a7a = 1;
+	////int double = 1;
+	//double fResult;
+	//int iResult, num1 = 18, num2 = 5;
+	//double a = int(18/5.0);
+	//cout << a;
+	
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//int main() {
+//    string str;
+//    getline(cin, str);
+//    string::reverse_iterator it;
+//    for (it = str.rbegin(); it != str.rend(); it++) {
+//        cout << *it;
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	cin >> a;
+//	while (a)
+//	{
+//		b = b * 10 + a % 10;
+//		a /= 10;
+//	}
+//	cout << b;
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//
+//int main()
+//{
+//	string str;
+//	set<char> s;
+//	getline(cin, str);
+//	for (int i = 0; i < str.length(); i++)
+//	{
+//		s.insert(str[i]);
+//	}
+//	cout << s.size() << endl;
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int n;
+//	int arr[10] = { 0 };
+//	int m = 0;
+//	cin >> n;
+//
+//	while (n)
+//	{
+//		if (arr[n % 10] == 0)
+//		{
+//			arr[n % 10]++;
+//			m = m * 10 + n % 10;
+//		}
+//		n /= 10;
+//	}
+//	cout << m << endl;
+//
+//	return 0;
+//}
