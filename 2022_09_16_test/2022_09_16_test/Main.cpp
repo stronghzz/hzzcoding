0a3c8f0dacbe19618b365b405a95a198b76ac5f8
stronghzz <760767745@qq.com>
2022-09-17 00:21:33 +0800

    练习代码

--- 2022_09_16_test/2022_09_16_test/Main.cpp
+++ 2022_09_16_test/2022_09_16_test/Main.cpp
@@ -8,7 +8,14 @@ int main()
 	//vector迭代器是普通指针
 	
 	vector<int> iv(2, 9);
-	cout <<
+	cout << iv[0] << endl;
+	cout << iv[1] << endl;
+	cout << "size=" << iv.size() << endl;
+	cout << "capacity=" << iv.capacity() << endl;
+	iv.push_back(1);
+	cout << "size=" << iv.size() << endl;
+	cout << "capacity" << iv.capacity() << endl;
+	
 
 	//bool a = true;
 	//cout << a;
