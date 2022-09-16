af7972d14f08fdc6ba9da8fd411b9a1d057c12c5
stronghzz <760767745@qq.com>
2022-09-17 00:00:36 +0800

    练习

---
+++ 2022_09_16_test/2022_09_16_test/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//vector迭代器是普通指针
+	
+	vector<int> iv(2, 9);
+	cout <<
+
+	//bool a = true;
+	//cout << a;
+	return 0;
+}
