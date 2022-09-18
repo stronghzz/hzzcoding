fe92eeef25ed53af59a8a690c5270f752d88a9af
stronghzz <760767745@qq.com>
2022-09-18 21:38:20 +0800

    练习代码

--- 2022_09_17_test/2022_09_17_test/Main.cpp
+++ 2022_09_17_test/2022_09_17_test/Main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <array>
 using namespace std;
 
 
 int main()
 {
-	vector<string> a;
-	a.push_back("hello,world");
-	cout << a[0] << endl;
-
-
+	array<int, 3> arr = { 1,2,3 };
+	array<char, 3> str = { 'a','b','c' };
+	array<int, 3> arr1{ 1,2,3 };
+	array<char, 3> str1{ 'a','b','c' };
 	return 0;
 }
+
+
+
+//int main()
+//{
+//	vector<string> a;
+//	a.push_back("hello,world");
+//	cout << a[0] << endl;
+//	return 0;
+//}
 
 
 //int main()
