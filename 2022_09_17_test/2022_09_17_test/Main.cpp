0f713b9a067886a0516dbf4659a09b8115fae0d5
stronghzz <760767745@qq.com>
2022-09-21 23:05:18 +0800

    练习代码

--- 2022_09_17_test/2022_09_17_test/Main.cpp
+++ 2022_09_17_test/2022_09_17_test/Main.cpp
@@ -5,36 +5,83 @@
 #include <algorithm>
 using namespace std;
 
-int main()
-{
-	int X = 0;
-	int Y = 0;
-	string str;
-	cin >> str;
-	while (1)
-	{
-		if (str.find(';') == string::npos)
-		{
-			break;
-		}
-		int i = str.find(';');
-		string temp = str.substr(0, (i-0));
-		if (temp.size() == 3 && temp.at(1) >= '0' && temp.at(1) <= '9' && temp.at(2) >= '0' && temp.at(2) <= '9'|| temp.size() == 2 && temp.at(1) >= '0' && temp.at(1) <= '9')
-		{
-			switch (temp.at(0))
-			{
-			case 'A': X -= stoi(temp.substr(1), 0, 10); break;
-			case 'D': X += stoi(temp.substr(1), 0, 10); break;
-			case 'W': Y += stoi(temp.substr(1), 0, 10); break;
-			case 'S': Y -= stoi(temp.substr(1), 0, 10); break;
-			default:break;
-			}
-		}
-		str = str.substr(i + 1);
-	}
-	cout << X << ',' << Y << endl;
-	return 0;
-}
+//int main()
+//{
+//	int N, m;
+//	cin >> N >> m;
+//	N /= 10;
+//	vector<vector<int> > prices(61, vector<int>(3,0));
+//	vector<vector<int> > priceMutiplyPriority(61, vector<int>(3.0));
+//	for (int i = 1; i <= m; i++)
+//	{
+//		int a, b, c;
+//		cin >> a >> b >> c;
+//		a /= 10;
+//		b *= a;
+//		if (c == 0)
+//		{
+//			prices[i][0] = a;
+//			priceMutiplyPriority[i][0] = b;
+//		}
+//		else
+//		{
+//			if (prices[c][1] == 0)
+//			{
+//				prices[c][1] = a;
+//				priceMutiplyPriority[c][1] = b;
+//			}
+//		}
+//	}
+//	vector<vector<int> > dp(m + 1, vector<int>(N + 1, 0));
+//	for (int i = 1; i <= m; ++i)
+//	{
+//		for (int j = 1; j <= N; ++j)
+//		{
+//			int a = prices[i][0], b = priceMutiplyPriority[i][0];
+//			int c = prices[i][1], d = priceMutiplyPriority[i][1];
+//			int e = prices[i][2], f = priceMutiplyPriority[i][2];
+//			dp[i][j] = j >= a ? max(dp[i - 1][j - a] + b, dp[i - 1][j]) : dp[i - 1][j];
+//			dp[i][j] = j >= (a + c) ? max(dp[i - 1][j - a - c] + b + d, dp[i][j]) : dp[i][j];
+//			dp[i][j] = j >= (a + e) ? max(dp[i - 1][j - a - e] + b + f, dp[i][j]) : dp[i][j];
+//			dp[i][j] = j >= (a + c + e) ? max(dp[i - 1][j - a - c - e] + b + d + f, dp[i][j]) : dp[i][j];
+// 		}
+//	}
+//	cout << dp[m][N] * 10 << endl;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int X = 0;
+//	int Y = 0;
+//	string str;
+//	cin >> str;
+//	while (1)
+//	{
+//		if (str.find(';') == string::npos)
+//		{
+//			break;
+//		}
+//		int i = str.find(';');
+//		string temp = str.substr(0, (i-0));
+//		if (temp.size() == 3 && temp.at(1) >= '0' && temp.at(1) <= '9' && temp.at(2) >= '0' && temp.at(2) <= '9'|| temp.size() == 2 && temp.at(1) >= '0' && temp.at(1) <= '9')
+//		{
+//			switch (temp.at(0))
+//			{
+//			case 'A': X -= stoi(temp.substr(1), 0, 10); break;
+//			case 'D': X += stoi(temp.substr(1), 0, 10); break;
+//			case 'W': Y += stoi(temp.substr(1), 0, 10); break;
+//			case 'S': Y -= stoi(temp.substr(1), 0, 10); break;
+//			default:break;
+//			}
+//		}
+//		str = str.substr(i + 1);
+//	}
+//	cout << X << ',' << Y << endl;
+//	return 0;
+//}
 
 //int main()
 //{	
