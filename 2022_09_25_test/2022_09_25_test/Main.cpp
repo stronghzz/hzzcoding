2824bfe361ac78c622dffe7fc9759e05cb2c0bdc
stronghzz <760767745@qq.com>
2022-09-25 21:15:49 +0800

    练习代码

---
+++ 2022_09_25_test/2022_09_25_test/Main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+
+//int main()
+//{
+//	int a[2][10] = {0};
+//	int b = 0;
+//	int c = 0;
+//	cin >> c;
+//	cin >> b;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		cin >> a[0][i];
+//	}
+//	for (int j = 0; j < 10; j++)
+//	{
+//		if (a[0][j] % b < c)
+//		{
+//			a[0][j] = a[0][j] % b;
+//		}
+//		else
+//		{
+//			a[1][j] = -1;
+//		}
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		for (int j = 1; j < 10; j++)
+//		{
+//			if()
+//		}
+//	}
+//	return 0;
+//}
