6559a547f8243cc2b7b6f0b70035ed3a97b06f67
stronghzz <760767745@qq.com>
2022-10-03 22:08:10 +0800

    练习代码

--- 2022_10_03_test/Main.c
+++ 2022_10_03_test/Main.c
@@ -4,8 +4,10 @@
 #define MAX_COLS 20
 #define MAX_INPUT 1000
 #define _CRT_SECURE_NO_WARNINGS
+
 int read_column_numbers(int columns[], int max);
 void rearrange(char* output, char const* input, int n_columns, int const columns[]);
+
 int main()
 {
 	int n_columns;
@@ -13,7 +15,7 @@ int main()
 	char input[MAX_INPUT];
 	char output[MAX_INPUT];
 	n_columns = read_column_numbers(columns, MAX_COLS);
-	while (gets(input) != NULL)
+	while (fgets(input,1000,stdin) != NULL)
 	{
 		printf("Original input:%s\n", input);
 		rearrange(output, input, n_columns, columns);
