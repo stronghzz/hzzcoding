a6ac8e1d88ed8207093ae0abda881965de77f20b
stronghzz <760767745@qq.com>
2023-03-02 15:24:32 +0800

    List

---
+++ 20230302/test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list
+{
+    int val;
+    struct list *next;
+} List;
+
+List *CreatList()
+{
+    List *l = (List *)malloc(sizeof(List));
+    l->next = NULL;
+    l->val = 0;
+    return l;
+}
+
+void AddLastNode(List *l, int val)
+{
+    List *_l = (List *)malloc(sizeof(List));
+    _l->val = val;
+    _l->next = NULL;
+    while (l->next != NULL)
+    {
+        l = l->next;
+    }
+    l->next = _l;
+}
+void ShowList(List *l)
+{
+    while (l->next != NULL)
+    {
+        printf("%d\n", l->val);
+        l = l->next;
+    }
+}
+int main()
+{
+    List *l = CreatList();
+    AddLastNode(l,1);
+    AddLastNode(l,2);
+    AddLastNode(l,3);
+    ShowList(l);
+    return 0;
+}
